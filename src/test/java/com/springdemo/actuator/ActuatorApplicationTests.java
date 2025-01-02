d963754e11618d19c33ac781ef69cfa504ce09d0
Ayush Khodankar <ayushkhodankar01@gmail.com>
2025-01-02 13:18:55 +0530

    first commit

---
+++ src/test/java/com/springdemo/actuator/ActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdemo.actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
